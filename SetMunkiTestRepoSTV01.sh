7c97d2c0dd5ee7f388ad1cc1243a73a52a9f81ab
niklas-skoldmark <niklas-skoldmark@users.noreply.github.com>
2014-08-21 13:25:22 +0200

    Delete SetMunkiTestRepoSTV01.sh

--- SetMunkiTestRepoSTV01.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Script to configure Munki to use the testrepo on stv01
-defaults write /Library/Preferences/ManagedInstalls SoftwareRepoURL "http://192.168.90.100/munki_repo/munki_repo"
